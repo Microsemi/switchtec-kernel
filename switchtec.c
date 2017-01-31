766518a7dc8b85c749761ab546484df7ff749770
Logan Gunthorpe <logang@deltatee.com>
2017-01-31 09:41:15 -0700

    Updated copyright year to 2017

--- switchtec.c
+++ switchtec.c
@@ -1,6 +1,6 @@
 /*
  * Microsemi Switchtec(tm) PCIe Management Driver
- * Copyright (c) 2016, Microsemi Corporation
+ * Copyright (c) 2017, Microsemi Corporation
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
