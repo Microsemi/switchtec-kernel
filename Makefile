68ff7156a190b15ef8a22e7ec3a6be5409db0ef1
Logan Gunthorpe <logang@deltatee.com>
2017-02-02 09:49:46 -0700

    Added pahole test

--- Makefile
+++ Makefile
@@ -19,3 +19,18 @@ install: modules_install
 
 %::
 	$(MAKE) -C $(KERNEL_SOURCES) M=$$PWD $@
+
+pahole64: pahole.c linux/switchtec_ioctl.h
+	gcc -O0 -g -I. $< -o $@
+
+pahole32: pahole.c linux/switchtec_ioctl.h
+	gcc -O0 -g -I. $< -o $@ -m32
+
+pahole: pahole64 pahole32
+	pahole pahole32 > pahole32.txt
+	pahole pahole64 > pahole64.txt
+	@cmp pahole32.txt pahole64.txt && echo "Arches Match" || \
+		echo "!!Arches don't match!!"
+
+clean::
+	rm -f pahole32 pahole64 pahole*.txt
---
+++ pahole.c
@@ -0,0 +1,25 @@
+/*
+ * Microsemi Switchtec(tm) PCIe Management Driver
+ * Copyright (c) 2017, Microsemi Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+#include <linux/switchtec_ioctl.h>
+
+void main(void)
+{
+	struct switchtec_ioctl_flash_info flash_info;
+	struct switchtec_ioctl_flash_part_info flash_part_info;
+	struct switchtec_ioctl_event_summary event_summary;
+	struct switchtec_ioctl_event_ctl event_ctl;
+	struct switchtec_ioctl_pff_port pff_port;
+}
